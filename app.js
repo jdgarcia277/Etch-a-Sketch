9e0cbcf37735070d504fa3a8655f6553658d4033
jdgarcia277 <jdgarcia277@yahoo.com>
2023-11-16 12:45:23 -0600

    Added function to screen size button. Also altered prev. code to make this work

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ function createDivs(number){
 
     let divSize = 480/number;
     let totalDivs = number * number;
+    let parent = document.querySelector("#playground");
+
+    //clear previous div's
+    parent.innerHTML = '';
     
     for (let i = 1; i <= totalDivs; i ++){
-
-        let parent = document.querySelector("#playground");
 
         let div = document.createElement("div");
         div.style.width = divSize + "px";
@@ -19,6 +21,84 @@ function createDivs(number){
    
 }
 
+function changeScreenSize(){
+
+    //read current screen size
+    let screenNum = getScreenSize();
+
+    const screenDisplayNode = document.querySelector(".size-display");
+
+    if (screenNum < 5){
+        screenNum++;
+    }
+    else {
+        screenNum = 1;
+    }
+
+    screenDisplayNode.textContent = "";
+
+    switch (screenNum){
+        case 1: 
+            screenDisplayNode.textContent = "16 x 16";
+            createDivs(16);
+            break;
+        case 2:
+            screenDisplayNode.textContent = "20 x 20";
+            createDivs(20);
+            break;
+        case 3:
+            screenDisplayNode.textContent = "10 x 10";
+            createDivs(10);
+            break;
+        case 4:
+            screenDisplayNode.textContent = "12 x 12";
+            createDivs(12);
+            break;
+        case 5:
+            screenDisplayNode.textContent = "14 x 14";
+            createDivs(14);
+            break;
+    }
+
+
+}
+
+function getScreenSize(){
+
+    let screenNum;
+    const screenDisplayNode = document.querySelector(".size-display");
+    const screenTextValue = screenDisplayNode.textContent;
+
+    switch (screenTextValue){
+
+        case "16 x 16": 
+            screenNum = 1;
+            break;
+        case "20 x 20":
+            screenNum = 2;
+            break;
+        case "10 x 10":
+            screenNum = 3;
+            break;
+        case "12 x 12":
+            screenNum = 4;
+            break;
+        case "14 x 14":
+            screenNum = 5;
+            break;
+        
+    }
+
+    return screenNum;
+
+}
+
+//event listener for screen-size
+const leftButton = document.querySelector(".left-control");
+if (leftButton !== null) {
+    leftButton.addEventListener("click", changeScreenSize);
+}
+
 document.addEventListener("DOMContentLoaded", (e) => {
-    createDivs(16);
+    changeScreenSize();
 })
